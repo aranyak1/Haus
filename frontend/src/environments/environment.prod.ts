df2df8b2ef3cfb8b88f7ddd4409a02583b7d795f
Aranyak Panigrahi <Aranyak Panigrahi>
2022-09-16 10:16:43 +0530

    modified env variables

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
-  apiUrl: 'https://haus.onrender.com',
+  apiUrl: 'http://localhost:8080/',
   apiVersion: 'api/v1/',
   Imagekitio: {
     publicKey: 'public_7yxYKRw/TRLq2AbI1L7xTXUV+Aw=',
     urlEndpoint: 'https://ik.imagekit.io/mu8g4or2k',
-    authenticationEndpoint: 'https://haus.onrender.com/auth',
+    authenticationEndpoint: 'http://localhost:8080//auth',
   },
   mapApiKey: 'S1bi375N577V4pv9zZMq',
   production: true,
