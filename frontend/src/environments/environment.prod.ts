a889379da4de7282f0f8212559e78f7533cea38d
Aranyak Panigrahi <Aranyak Panigrahi>
2022-09-16 08:44:23 +0530

    modified env file

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  apiUrl: 'https://haus.onrender.com',
+  apiVersion: 'api/v1/',
+  Imagekitio: {
+    publicKey: 'public_7yxYKRw/TRLq2AbI1L7xTXUV+Aw=',
+    urlEndpoint: 'https://ik.imagekit.io/mu8g4or2k',
+    authenticationEndpoint: 'https://haus.onrender.com/auth',
+  },
+  mapApiKey: 'S1bi375N577V4pv9zZMq',
+  production: true,
 };
