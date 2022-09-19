1bc253818725609b65b416f432a25b840edcb366
Aranyak Panigrahi <Aranyak Panigrahi>
2022-09-19 09:19:56 +0530

    updated environment.ts file

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
-  apiUrl: 'https://haus.onrender.com',
+  apiUrl: 'https://haus.onrender.com/',
   apiVersion: 'api/v1/',
   Imagekitio: {
     publicKey: 'public_7yxYKRw/TRLq2AbI1L7xTXUV+Aw=',
     urlEndpoint: 'https://ik.imagekit.io/mu8g4or2k',
-    authenticationEndpoint: 'https://haus.onrender.com/auth',
+    authenticationEndpoint: 'https://haus.onrender.com//auth',
   },
   mapApiKey: 'S1bi375N577V4pv9zZMq',
   production: true,
